86fb8618c4ed78b67ed8dce2aa6cdd8466eaffbe
Yuzyzy88 <ayu.sulistiowati@protonmail.com>
2020-04-15 15:33:39 +0700

    Add comment.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Render to app
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
